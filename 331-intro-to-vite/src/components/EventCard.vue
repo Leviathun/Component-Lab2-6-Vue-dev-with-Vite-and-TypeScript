763a931bc63ac04ac548004ad09b3b87011243db
Leviathun <153574902+Leviathun@users.noreply.github.com>
2024-07-04 18:41:55 +0700

    6 update EventCard.vue

--- 331-intro-to-vite/src/components/EventCard.vue
+++ 331-intro-to-vite/src/components/EventCard.vue
@@ -1,11 +1,29 @@
 <script setup lang="ts">
-defineProps<{
-  msg: string
-}>()
+import { describe } from 'node:test';
+import { ref } from 'vue'
+//defineProps<{
+//  msg: string
+//}>()
+const event = ref({
+  id: 5928101,
+  category: 'animal welfare',
+  title: 'Cat Adoption Day',
+  description: 'Find your new feline friend at this event.',
+  location: 'Meow Town',
+  date: 'January 28, 2022',
+  time: '12:00',
+  petsAllowed: true,
+  organizer: 'Kat Laydee'
+})
 </script>
 
 <template>
-  <div class="event-class"></div>
+  <div class="event-class">
+    <div class="event-card">
+      <h2>{{ event.title }}</h2>
+      <span>@{{ event.time }} on {{ event.date }}</span>
+    </div>
+  </div>
 </template>
 
 <style scoped>
