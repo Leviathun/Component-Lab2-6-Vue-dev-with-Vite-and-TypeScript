13954ea21cf75d57360d996cce780515b2a14dbb
Leviathun <153574902+Leviathun@users.noreply.github.com>
2024-07-04 19:08:28 +0700

    8.3 Create type.ts

--- 331-intro-to-vite/src/components/EventCard.vue
+++ 331-intro-to-vite/src/components/EventCard.vue
@@ -1,20 +1,21 @@
 <script setup lang="ts">
-import { describe } from 'node:test';
 import { ref } from 'vue'
-//defineProps<{
-//  msg: string
-//}>()
-const event = ref({
-  id: 5928101,
-  category: 'animal welfare',
-  title: 'Cat Adoption Day',
-  description: 'Find your new feline friend at this event.',
-  location: 'Meow Town',
-  date: 'January 28, 2022',
-  time: '12:00',
-  petsAllowed: true,
-  organizer: 'Kat Laydee'
-})
+
+import Event from '@/types/Event'
+defineProps<{
+  event: Event
+}>()
+/*  const event = ref({
+    id: 5928101,
+    category: 'animal welfare',
+    title: 'Cat Adoption Day',
+    description: 'Find your new feline friend at this event.',
+    location: 'Meow Town',
+    date: 'January 28, 2022',
+    time: '12:00',
+    petsAllowed: true,
+    organizer: 'Kat Laydee'
+  })*/
 </script>
 
 <template>
