025f91646d085c73d1734f44455dca1211c828c3
Leviathun <153574902+Leviathun@users.noreply.github.com>
2024-07-05 09:25:48 +0700

    9 Add text element

--- 331-intro-to-vite/src/views/HomeView.vue
+++ 331-intro-to-vite/src/views/HomeView.vue
@@ -40,6 +40,8 @@
 </script>
 
 <template>
+  <h1>Event For Good</h1>
+  <!-- new element -->
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event"/>
   </div>
