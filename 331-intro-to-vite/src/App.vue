372cf713fce629110a4140c20cbaae5d2d8a67e7
Leviathun <153574902+Leviathun@users.noreply.github.com>
2024-07-26 17:21:40 +0700

    1.9 Update router-link in App.vue

--- 331-intro-to-vite/src/App.vue
+++ 331-intro-to-vite/src/App.vue
@@ -7,9 +7,9 @@ import { RouterLink, RouterView } from 'vue-router'
     <header>
       <div class="wrapper">
         <nav>
-          <RouterLink to="/">Event</RouterLink> |
-          <RouterLink to="/about">About</RouterLink> |
-          <RouterLink to="/student">Student</RouterLink>
+          <RouterLink :to="{ name: 'event-list-view' }">Event</RouterLink> |
+          <RouterLink :to="{ name: 'about' }">About</RouterLink> |
+          <RouterLink :to="{ name: 'student' }">Student</RouterLink>
         </nav>
       </div>
     </header>
