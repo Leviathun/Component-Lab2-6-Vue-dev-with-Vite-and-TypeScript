2de8671c19f894856fc67fc5da637faed2bda9d7
Leviathun <153574902+Leviathun@users.noreply.github.com>
2024-08-07 17:10:07 +0700

    2.8 Check last page  remove NextPage

--- 331-intro-to-vite/src/views/EventListView.vue
+++ 331-intro-to-vite/src/views/EventListView.vue
@@ -5,7 +5,11 @@
   import EventService from '@/services/EventService';
 
   const events = ref<Event[] | null>(null)
-
+  const totalEvents = ref(0)
+  const hasNexPage = computed(() => {
+    const totalPages = Math.ceil(totalEvents.value / 2)
+    return page.value < totalPages
+  })
   const props = defineProps ({
     page: {
      type: Number,
@@ -20,6 +24,7 @@ const page = computed (() => props.page)
       EventService.getEvents(2, page.value)
         .then((response) => {
           events.value = response.data
+          totalEvents.value = response.headers['x-total-count']
         })
         .catch((error) => {
           console.error('There was an error!', error)
@@ -41,7 +46,10 @@ const page = computed (() => props.page)
     >Prev Page</RouterLink
   >
 
-  <RouterLink :to="{ name: 'event-list-view', query: { page: page + 1 } }" rel="next"
+  <RouterLink 
+    :to="{ name: 'event-list-view', query: { page: page + 1 } }" 
+    rel="next"
+    v-if="hasNexPage"
     >Next Page</RouterLink
   >
   
