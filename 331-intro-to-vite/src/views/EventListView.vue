d97b61b313b6fd5261f555309b25f8e3c543bb8f
Leviathun <153574902+Leviathun@users.noreply.github.com>
2024-08-07 16:55:48 +0700

    2.7 Detail changed

--- 331-intro-to-vite/src/views/EventListView.vue
+++ 331-intro-to-vite/src/views/EventListView.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
   import EventCard from '@/components/EventCard.vue';
   import { type Event } from '@/types'
-  import { ref , onMounted, computed } from 'vue'
+  import { ref , onMounted, computed, watchEffect } from 'vue'
   import EventService from '@/services/EventService';
 
   const events = ref<Event[] | null>(null)
@@ -15,13 +15,15 @@
 
 const page = computed (() => props.page)
   onMounted (() => {
-  EventService.getEvents(2, page.value) 
-    .then ((response) => {
-      //console.log(response.data)
-      events.value = response.data
-    })
-    .catch((error) => {
-      console.error ('There was an error!', error)
+    watchEffect(() => {
+      events.value = null
+      EventService.getEvents(2, page.value)
+        .then((response) => {
+          events.value = response.data
+        })
+        .catch((error) => {
+          console.error('There was an error!', error)
+        })
     })
   })
 </script>
