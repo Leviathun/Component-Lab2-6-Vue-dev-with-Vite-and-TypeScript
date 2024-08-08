3ecddd562dfecd045d9fa73d7aa2bdc1886f638a
Leviathun <153574902+Leviathun@users.noreply.github.com>
2024-08-09 03:54:29 +0700

    1.4-1.5 Register flash message

--- 331-intro-to-vite/src/views/event/RegisterView.vue
+++ 331-intro-to-vite/src/views/event/RegisterView.vue
@@ -2,6 +2,7 @@
 import { toRefs, defineProps } from 'vue'
 import { type Event } from '@/types'
 import { useRouter } from 'vue-router';
+import { useMessageStore } from '@/stores/message'
 
 const props = defineProps<{
     event: Event
@@ -10,11 +11,14 @@ const props = defineProps<{
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const { event } = toRefs(props)
 const router = useRouter()
+const store = useMessageStore ()
 const register = () => {
-  // If the registration API call successful
-  // Push back to the event details view
-  router.push({ name: 'event-detail-view' })
-}
+  store.updateMessage('You are successuflly registered for ' + props.event.title)
+  setTimeout (() => {
+    store.resetMessage()
+  }, 3000)
+  router.push({ name: 'event-detail-view', params: { id: props.event.id } })
+  }
 </script>
 
 <template>
