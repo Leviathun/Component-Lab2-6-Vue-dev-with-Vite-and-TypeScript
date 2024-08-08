3af7c0c22bc2391c530a2243ba46c68cdd0173f2
Leviathun <153574902+Leviathun@users.noreply.github.com>
2024-08-09 03:58:21 +0700

    1.6 flash message for edit page

--- 331-intro-to-vite/src/views/event/EditView.vue
+++ 331-intro-to-vite/src/views/event/EditView.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import { toRefs, defineProps } from 'vue'
 import { type Event } from '@/types'
-
+import { useRouter } from 'vue-router';
+import { useMessageStore } from '@/stores/message'
 
 const props = defineProps<{
     event: Event
@@ -9,9 +10,18 @@ const props = defineProps<{
 }>()
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const { event } = toRefs(props)
+const router = useRouter()
+const store = useMessageStore ()
+const edit = () => {
+  store.updateMessage('The data has been updated for ' + props.event.title)
+  setTimeout (() => {
+    store.resetMessage()
+  }, 3000)
+  router.push({ name: 'event-detail-view', params: { id: props.event.id } })
+  }
 </script>
 
 <template>
     <p>Edit event here</p>
+    <button @click="edit">Edit</button>
 </template>
-    
