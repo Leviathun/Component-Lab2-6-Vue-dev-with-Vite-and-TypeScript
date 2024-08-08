139d384ceac1745655c797e5e7c6080a8ef861f4
Leviathun <153574902+Leviathun@users.noreply.github.com>
2024-08-08 18:17:11 +0700

    4. RegisterView routing by program

--- 331-intro-to-vite/src/views/event/RegisterView.vue
+++ 331-intro-to-vite/src/views/event/RegisterView.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { toRefs, defineProps } from 'vue'
 import { type Event } from '@/types'
-
+import { useRouter } from 'vue-router';
 
 const props = defineProps<{
     event: Event
@@ -9,8 +9,15 @@ const props = defineProps<{
 }>()
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const { event } = toRefs(props)
+const router = useRouter()
+const register = () => {
+  // If the registration API call successful
+  // Push back to the event details view
+  router.push({ name: 'event-detail-view' })
+}
 </script>
 
 <template>
     <p>Register event here</p>
+    <button @click="register">Register</button>
 </template>
