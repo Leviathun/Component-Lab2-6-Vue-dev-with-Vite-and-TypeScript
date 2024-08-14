08205b454721c4c18cc062363ab1b5440f0e9312
Leviathun <153574902+Leviathun@users.noreply.github.com>
2024-08-14 22:51:29 +0700

    9 Exact-active-class

--- 331-intro-to-vite/src/App.vue
+++ 331-intro-to-vite/src/App.vue
@@ -13,10 +13,10 @@ const { message } = storeToRefs(store)
        <h4>{{ message }}</h4>
       </div>
       <div class="wrapper">
-        <nav>
-          <RouterLink :to="{ name: 'event-list-view' }">Event</RouterLink> |
-          <RouterLink :to="{ name: 'about' }">About</RouterLink> |
-          <RouterLink :to="{ name: 'student' }">Student</RouterLink>
+        <nav class="py-6">
+          <RouterLink class="font-bold text-gray-700" exact-active-class="text-green-500" :to="{ name: 'event-list-view' }">Event</RouterLink> |
+          <RouterLink class="font-bold text-gray-700" exact-active-class="text-green-500" :to="{ name: 'about' }">About</RouterLink> |
+          <RouterLink class="font-bold text-gray-700" exact-active-class="text-green-500" :to="{ name: 'student' }">Student</RouterLink>
         </nav>
       </div>
     </header>
