84ed9611ac980cb94ea674ee4d263b6c09f892ba
Leviathun <153574902+Leviathun@users.noreply.github.com>
2024-08-14 22:23:43 +0700

    5 Update class event-class

--- 331-intro-to-vite/src/components/EventCard.vue
+++ 331-intro-to-vite/src/components/EventCard.vue
@@ -19,7 +19,7 @@ defineProps<{
 
 <template>
   <RouterLink class="event-link" :to="{ name: 'event-detail-view', params: { id: event.id}}">
-    <div class="event-card hover:scale-101 hover:shadow-sp">
+    <div class="cursor-pointer border border-gray-600 p-4 w-64 mb-6 hover:scale-101 hover:shadow-sp">
       <h2>{{ event.title }}</h2>
       <span>@{{ event.time }} on {{ event.date }}</span>
     </div>
