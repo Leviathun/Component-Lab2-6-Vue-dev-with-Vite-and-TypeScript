a40ed23767cb129da3d32bb69b358be08cc0b95c
Leviathun <153574902+Leviathun@users.noreply.github.com>
2024-08-16 10:20:32 +0700

    12.8 update code

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,7 @@ const { message } = storeToRefs(store)
       <div id="flashMessage" class="animate-fade" v-if="message">
        <h4>{{ message }}</h4>
       </div>
+      <h1>Deploy with Vercel</h1>
       <div class="wrapper">
         <nav class="py-6 p-[30px]">
           <RouterLink class="font-bold text-gray-700" exact-active-class="text-green-500" :to="{ name: 'event-list-view' }">Event</RouterLink> |
