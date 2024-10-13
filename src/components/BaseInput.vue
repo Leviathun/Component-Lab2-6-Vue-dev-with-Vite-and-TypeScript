9ece07e4c3c798c40a6d37a40e2ca0b7dc55e94b
Leviathun <153574902+Leviathun@users.noreply.github.com>
2024-10-13 17:50:39 +0700

    5.1 Create BaseInput.vue component

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const modelValue = defineModel()
+interface BaseInputProps {
+    label: string
+}
+const props = withDefaults(defineProps<BaseInputProps>(), {
+    label: ''
+})
+</script>
+<template>
+    <label v-if="label">
+        {{ props.label }}
+    </label>
+    <input class="mb-6" v-bind="$attrs" v-model="modelValue" :placeholder="label" />
+</template>
